4b969917c9dbf705db219d8e16e0509380fcdf0e
Kicamon <kicamonice@gmail.com>
2024-09-05 09:16:28 +0800

    update

--- lua/im-switch/init.lua
+++ lua/im-switch/init.lua
@@ -4,50 +4,27 @@ local input_toggle = 1
 local ts_utils = require('nvim-treesitter.ts_utils')
 
 local function En()
-  local input_status = tonumber(io.popen(switch.check):read("*all"))
-  if (input_status == 2) then
+  local input_status = tonumber(io.popen(switch.check):read('*all'))
+  if input_status == 2 then
     input_toggle = 1
     vim.fn.system(switch.en)
   end
 end
 
 local function Zh()
-  local input_status = tonumber(io.popen(switch.check):read("*all"))
-  if (input_status ~= 2 and input_toggle == 1) then
+  local input_status = tonumber(io.popen(switch.check):read('*all'))
+  if input_status ~= 2 and input_toggle == 1 then
     vim.fn.system(switch.zh)
     input_toggle = 0
   end
 end
 
-local md = {
-  "language",
-  "fenced_code_block_delimiter",
-  "link_destination",
-  "code_fence_content",
-  "fenced_code_block",
-  "latex_block",
-}
-
-local md_code = {
-  "chunk",            --lua
-  "translation_unit", --c/cpp
-  "module",           --python
-}
-
 local function is_not_in_code_block() --markdown
-  local node_cursor = ts_utils.get_node_at_cursor()
-  for _, node_type in ipairs(md) do
-    if node_cursor and node_cursor:type() == node_type then
+  local node_cursor = vim.treesitter.get_captures_at_cursor(0)
+  for _, v in pairs(node_cursor) do
+    if string.find(v, 'markup.link') or string.find(v, 'markup.raw.block') then
       return false
     end
-  end
-  while node_cursor do
-    for _, node_type in ipairs(md_code) do
-      if node_cursor and node_cursor:type() == node_type then
-        return false
-      end
-    end
-    node_cursor = node_cursor:parent()
   end
   return true
 end
@@ -61,34 +38,34 @@ local function filetype_checke()
 end
 
 local function setup(opt)
-  switch = vim.tbl_deep_extend("force", {
+  switch = vim.tbl_deep_extend('force', {
     text = {
-      "*.md",
-      "*.txt"
+      '*.md',
+      '*.txt',
     },
     code = {
-      "*",
+      '*',
     },
-    en = "fcitx5-remote -c",
-    zh = "fcitx5-remote -o",
-    check = "fcitx5-remote",
+    en = 'fcitx5-remote -c',
+    zh = 'fcitx5-remote -o',
+    check = 'fcitx5-remote',
   }, opt or {})
 
-  vim.api.nvim_create_autocmd("InsertLeave", {
-    pattern = "*",
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    pattern = '*',
     callback = function()
       En()
-    end
+    end,
   })
-  vim.api.nvim_create_autocmd("InsertEnter", {
+  vim.api.nvim_create_autocmd('InsertEnter', {
     pattern = switch.text,
     callback = function()
       if filetype_checke() then
         Zh()
       end
-    end
+    end,
   })
-  vim.api.nvim_create_autocmd("InsertEnter", {
+  vim.api.nvim_create_autocmd('InsertEnter', {
     pattern = switch.code,
     callback = function()
       local current_pos = vim.fn.getcurpos()
@@ -96,13 +73,16 @@ local function setup(opt)
       vim.fn.setpos('.', current_pos)
       local previous_node = ts_utils.get_node_at_cursor()
 
-      if previous_node and (previous_node:type() == 'comment' or previous_node:type() == 'comment_content') then
+      if
+        previous_node
+        and (previous_node:type() == 'comment' or previous_node:type() == 'comment_content')
+      then
         Zh()
       end
-    end
+    end,
   })
 
-  vim.api.nvim_create_autocmd("TextChangedI", {
+  vim.api.nvim_create_autocmd('TextChangedI', {
     pattern = switch.code,
     callback = function()
       if (vim.bo.filetype == 'python' or vim.bo.filetype == 'sh') and vim.fn.line('.') == 1 then
@@ -112,15 +92,18 @@ local function setup(opt)
       current_pos[3] = current_pos[3] - 1
       vim.fn.setpos('.', current_pos)
       local previous_node = ts_utils.get_node_at_cursor()
-      if previous_node and (previous_node:type() == 'comment' or previous_node:type() == 'comment_content') then
+      if
+        previous_node
+        and (previous_node:type() == 'comment' or previous_node:type() == 'comment_content')
+      then
         Zh()
       end
       current_pos[3] = current_pos[3] + 1
       vim.fn.setpos('.', current_pos)
-    end
+    end,
   })
 end
 
 return {
-  setup = setup
+  setup = setup,
 }
